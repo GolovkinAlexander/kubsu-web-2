5c398c85993b407fbf3f9903ce96d2c5257af2a1
Alexander Golovkin <55evg77@gmail.com>
2023-05-11 23:14:59 +0300

    не нужно, но пусть будет

--- 6/login.php
+++ 6/login.php
@@ -11,35 +11,47 @@ if (!empty($_SESSION['login'])) {
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     ?>
-<html>
+<!DOCTYPE html>
+<html lang="ru">
+
 <head>
-    <link rel="icon" type="image/x-icon" href="favicon.svg">
+	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <title>Логин</title>
+    <title>Авториация</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js" defer></script>
+    <style>
+    .wrapper {
+    width: 400px;
+    margin:0 auto;
+    margin-top:12%;
+    padding:10px;
+    background: #fc0; 
+    border: 3px solid #000000;
+    border-radius: 10px;
+    }
+    </style>
 </head>
+
+
 <body>
-<div class="col col-10 col-md-11" id="forma">
+
+<div class="wrapper" id="forma">
         <form id="form1" action="" method="POST">
-            <div class="form-group">
+        
                 <label for="name">Логин</label>
                 <input name="login" id="name" class="form-control" placeholder="Введите ваш логин" ">
-            </div>
-            <div class="form-group">
+                
                 <label for="pwd">Пароль</label>
-
                 <input name="pass" class="form-control" id="pwd" placeholder="Введите ваш пароль" >
-
-            </div>
-           
+               <br>
             <input type="submit" id="btnend" class="btn btn-primary" value="Отправить">
+            
         </form>
     </div>
     </div>
 </body>
-
 
 </html>
 <?php
